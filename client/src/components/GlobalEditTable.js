ec482df30fccdb59785f6c2311eb202b067a7445
JonCabot <jfc0053@gmail.com>
2022-07-20 06:32:11 -0500

    added react router and mui dependencies

---
+++ client/src/components/GlobalEditTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+
+function GlobalEditTable() {  //import students objects, fetch one level higher?
+
+  //define columns and rows with mapping over student object
+  return (
+    <div>GlobalEditTable
+
+    </div>
+  )
+}
+
+export default GlobalEditTable

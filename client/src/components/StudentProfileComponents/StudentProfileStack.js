f7b152ef8b01ea18ce1b27704bd82598f82f98bd
JonCabot <jfc0053@gmail.com>
2022-07-31 09:56:42 -0500

    styling grid in student profile

--- client/src/components/StudentProfileComponents/StudentProfileStack.js
+++ client/src/components/StudentProfileComponents/StudentProfileStack.js
@@ -1,17 +1,40 @@
-import { Stack, Grid } from '@mui/material'
+import { Grid, Typography, Box, Stack } from '@mui/material'
+import HouseIcon from '@mui/icons-material/House';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import React from 'react'
 
+//map over is_owned_or_rented to create additional properties
+//map over snack, music, and investments to produce a history of purchases
 
 function StudentProfileStack() {
   return (
-    <Stack>
-      <Grid>
-        Desk Property
-      </Grid>
-      <Grid>
-        Music Card, etc...
-      </Grid>
-    </Stack>
+    <Grid container rowSpacing={1}  >
+        <Grid item xs={12} display='flex' border='2px solid'>
+          <Box>
+            <HouseIcon />
+          </Box>
+          <Stack>
+            <Box width='450px'>
+              <Typography fontWeight={'bold'} >Desk Rented:  #1</Typography>
+            </Box>
+            <Box width='450px' display='flex'>
+              <Typography fontWeight={'bold'} >Desk Owned: #2</Typography> &nbsp;
+              <Typography>timestamp  , Purchased for $  , Value $   .</Typography>
+            </Box>
+          </Stack>
+          
+        </Grid>
+
+        <Grid item xs={12} display='flex' border='2px solid'>
+          <Box>
+            <MusicNoteIcon />
+          </Box>
+          <Box>
+            <Typography fontWeight={'bold'}>Music Card: </Typography>
+            <Typography>timestamp  , Purchased for $  , Value $   .</Typography>
+          </Box>
+        </Grid>
+    </Grid>
   )
 }
 
--- client/src/pages/StudentProfilePage.js
+++ client/src/pages/StudentProfilePage.js
@@ -12,22 +12,24 @@ function StudentProfilePage() {
       <Wrapper component={'div'} style={{ borderRadius: '7px' }}>
           <Typography variant='h5'>Student Profile</Typography>
       </Wrapper>
+      <BalanceWrapper>
+          <Typography component={'div'} sx={{width: 'fit-content', border: '2px solid'}}>Balance: $  "student.balance"</Typography>
+      </BalanceWrapper>
       <Box>
-      </Box>
-      <Box>
-        <BalanceWrapper component={'div'}>
-          <Typography>Balance: $  "student.balance"</Typography>
-        </BalanceWrapper>
         <Box display={'flex'}>
           <Box>
-            <Box>Profile pic <Avatar /></Box>
+            <Box>
+              Profile pic <Avatar />
+            </Box>
             <GoalsWrapper>
-              <Box>Goals: "student.goals"</Box>
+              <Box>
+                Goals: "student.goals"
+              </Box>
             </GoalsWrapper>
           </Box>
-          <Box>
-            <StudentProfileStack />
-          </Box>
+            <Box>
+             <StudentProfileStack />
+            </Box>
         </Box>
       </Box>
     </Box>
@@ -45,9 +47,11 @@ const Wrapper = styled.section`
 `;
 
 const BalanceWrapper = styled.section`
-  text-align: center;
-  width: 200px;
-  border: 2px solid
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-right: 30px;
+  margin-top: 15px
 `;
 
 const GoalsWrapper = styled.section`

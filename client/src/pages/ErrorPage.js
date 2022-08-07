acbc9cd1c8be30febe2024c7a7eacd866dfe03c6
JonCabot <jfc0053@gmail.com>
2022-08-07 07:04:55 -0500

    updated error page style

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import ErrorIcon from '@mui/icons-material/Error';
+import { Box, Stack } from '@mui/material';
+
 
 function ErrorPage() {
   return (
-    <div>404 Page Not Found, Bad Route</div>
+    <Box display="flex" justifyContent="center" alignItems="center" marginTop={7}>
+      <Stack  spacing={2}>
+        <Box margin='auto'><ErrorIcon sx={{ fontSize: 40 }}/></Box>
+        <Box>Error 404: Page Not Found.  URL may be incorrect.</Box>
+      </Stack>
+    </Box>
+    
   )
 }
 
